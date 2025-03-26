6b3b12162ccb509be5ce421e8d72d009a3ed3f2a
unknown <anasirfan101010@gmail.com>
2025-03-26 10:08:06 +0500

    lighting carousel uncomment

--- src/app/page.js
+++ src/app/page.js
@@ -42,7 +42,7 @@ export default function Home() {
       <SplashScreen />
       <Header />
       <HeroSection />
-      {/* <LightingCarousel /> */}
+      <LightingCarousel />
       {/* <MotiveSection /> */}
       {/* <InteractiveStory /> */}
       {/* <CubeAnimation /> */}
