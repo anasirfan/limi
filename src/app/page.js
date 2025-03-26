3e9e7421005c025c1e0ee9db42fd9bc105e61829
unknown <anasirfan101010@gmail.com>
2025-03-26 10:00:40 +0500

    comments sections

--- src/app/page.js
+++ src/app/page.js
@@ -39,17 +39,17 @@ export default function Home() {
 
   return (
     <main>
-      <SplashScreen />
+      {/* <SplashScreen /> */}
       <Header />
       <HeroSection />
       {/* <LightingCarousel /> */}
-      <MotiveSection />
+      {/* <MotiveSection />
       <InteractiveStory />
     <CubeAnimation />
       <ModelSection />
       <LightingScene />
       {!isMobile && <MouseTrail />}
-      <Footer />
+      <Footer /> */}
       <ScrollToTop />
     </main>
   );
