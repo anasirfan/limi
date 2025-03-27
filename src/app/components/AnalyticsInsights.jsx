99f02b7cf693c153c9abc84c240f69127c160044
unknown <anasirfan101010@gmail.com>
2025-03-27 11:39:37 +0500

    hub image changed

--- src/app/components/AnalyticsInsights.jsx
+++ src/app/components/AnalyticsInsights.jsx
@@ -123,7 +123,7 @@ const AnalyticsInsights = () => {
       description: "Central control unit for all LIMI lighting products",
       availability: "Available for pre-orders",
       progress: 90,
-      image: "https://images.unsplash.com/photo-1565814329452-e1efa11c5b89?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80",
+      image: "https://images.unsplash.com/photo-1558002038-1055907df827?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
     },
     {
       stage: "Testing Phase",
